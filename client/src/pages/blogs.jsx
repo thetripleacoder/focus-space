92b3ab0a9a58bd23f46bd94077911604c3ea3593
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2025-08-15 11:57:14 +0800

    refactor(blogs): sort blogs by creation date
    - Sorted blogs by createdAt descending
    - Display newest blogs first

--- client/src/pages/blogs.jsx
+++ client/src/pages/blogs.jsx
@@ -4,10 +4,16 @@ import CreateBlog from './createBlog';
 
 export default function Blogs() {
   const blogs = useSelector((state) => state.blogs);
+
+  // Sort by createdAt descending (newest first)
+  const sortedBlogs = [...blogs].sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
+
   return (
     <>
       <CreateBlog />
-      <BlogList blogs={blogs} />
+      <BlogList blogs={sortedBlogs} />
     </>
   );
 }
